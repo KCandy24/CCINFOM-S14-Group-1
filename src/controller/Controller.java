ce4bf2e8a57a3667527d4a7550ca407c1bfb98ee
Kcandy <83863207+KCandy24@users.noreply.github.com>
2024-11-11 23:33:15 +0800

    Added: JDBC Connectivity

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -2,21 +2,18 @@ package src.controller;
 
 import src.model.AnimeSystem;
 import src.view.gui.TopView;
-
 /**
  * The controller communicates between the model and the view.
  */
 public class Controller {
     // Declare listeners
     TitlesSearchBoxListener titlesSearchBoxListener;
-
     /**
      * Initializes the listeners to listen to the view.
      */
     public Controller(AnimeSystem as, TopView tv) {
         // Initialize listeners
         titlesSearchBoxListener = new TitlesSearchBoxListener(as, tv);
-
         // Set listeners in topView
         tv.setSearchBoxListener(titlesSearchBoxListener);
     }
--- src/model/AnimeSystem.java
+++ src/model/AnimeSystem.java
@@ -1,37 +1,65 @@
 package src.model;
 
 import java.util.ArrayList;
+import java.sql.*;
 
 /**
  * TODO: Actually use a SQL Database instead of hardcoding these lists
  */
 public class AnimeSystem {
-    private String[] titles = {
-            "Ao no Kanata Four Rhythms Across the Blue",
-            "Cory in the House",
-            "Dragon Ball Z",
-            "Goblin Slayer",
-            "Konosuba: God's Gift on this Wonderful World",
-            "Love Live School Idol Project",
-            "Love Live Sunshine",
-            "Mayo Chiki!",
-            "Miss Kobayashi's Dragon Maid",
-            "Nichijou",
-            "Sankarea",
-            "Shrek",
-            "Tokyo Ghoul",
-            "The Helpful Fox Senko-san",
-            "Trinity Seven"
-    };
+
+    public final static String PATH = "jdbc:mysql://localhost:3306/dbanime";
+    public final static String USER = "root";
+    public final static String PASS = "1234";
+
+    Connection dbConnection;
+    Statement dbStatement;
+    ResultSet dbResultSet;
+    private ArrayList<String> titles;
+            // "Ao no Kanata Four Rhythms Across the Blue",
+            // "Cory in the House",
+            // "Dragon Ball Z",
+            // "Goblin Slayer",
+            // "Konosuba: God's Gift on this Wonderful World",
+            // "Love Live School Idol Project",
+            // "Love Live Sunshine",
+            // "Mayo Chiki!",
+            // "Miss Kobayashi's Dragon Maid",
+            // "Nichijou",
+            // "Sankarea",
+            // "Shrek",
+            // "Tokyo Ghoul",
+            // "The Helpful Fox Senko-san",
+            // "Trinity Seven"
 
     public AnimeSystem() {
-
+        titles = new ArrayList<String>();
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            dbConnection = DriverManager.getConnection(PATH, USER, PASS);
+            dbStatement = dbConnection.createStatement();
+        } catch (Exception e) {
+            System.err.println("Unable to establish connection");
+            e.printStackTrace();
+        }
+        this.getTitles();
     }
 
     // Titles
     
-    public String[] getTitles() {
-        return titles;
+    public void getTitles() {
+        try {
+            dbResultSet = dbStatement.executeQuery("SELECT * FROM animes");
+            while (dbResultSet.next()) {
+                String title = dbResultSet.getString("title");
+                if (!title.equals("")) {
+                    titles.add(title);
+                }
+            }
+        } catch (Exception e) {
+            System.err.println("Query to 'dbanime' Failed");
+            e.printStackTrace();
+        }
     }
 
     public String minimize(String string) {
