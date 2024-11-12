97f2ec384a72c2865b341c4c6a016a2b54be90db
Kcandy <83863207+KCandy24@users.noreply.github.com>
2024-11-12 23:34:33 +0800

    Added: AnimeSystem now fetches Procedures and can execute procedures as needed

--- src/model/AnimeSystem.java
+++ src/model/AnimeSystem.java
@@ -11,11 +11,12 @@ public class AnimeSystem {
 
     public final static String PATH = "jdbc:mysql://localhost:3306/dbanime";
 
-    Connection dbConnection;
-    Statement dbStatement;
-    ResultSet dbResultSet;
-    ResultSetMetaData dbMetaData;
+    private Connection dbConnection;
+    private Statement dbStatement;
+    private ResultSet dbResultSet;
+    private ResultSetMetaData dbMetaData;
     private ArrayList<String> titles;
+    private ArrayList<String> procedureList;
             // "Ao no Kanata Four Rhythms Across the Blue",
             // "Cory in the House",
             // "Dragon Ball Z",
@@ -43,6 +44,23 @@ public class AnimeSystem {
             e.printStackTrace();
         }
         this.getTitles();
+        procedureList = new ArrayList<String>();
+        try {
+            dbResultSet = dbStatement.executeQuery
+            (
+                "SELECT * " +
+                "FROM information_schema.routines " +
+                "WHERE routine_type = 'PROCEDURE' " +
+                    "AND ROUTINE_SCHEMA = 'dbanime';"
+            );
+            while (dbResultSet.next()) {
+                procedureList.add(dbResultSet.getString("SPECIFIC_NAME"));
+                System.out.println(dbResultSet.getString("SPECIFIC_NAME"));
+            }
+        } catch (Exception e) {
+            System.err.println("Failed to fetch procedure list.");
+            e.printStackTrace();
+        }
     }
 
     // Titles
@@ -57,19 +75,19 @@ public class AnimeSystem {
                 }
             }
         } catch (Exception e) {
-            System.err.println("Query to 'dbanime' Failed");
+            System.err.println("Query to 'dbanime' Failed.");
             e.printStackTrace();
         }
     }
 
-    private static String[] getColumnNames(ResultSetMetaData dbMetaData) throws SQLException {
-        int columnCount = dbMetaData.getColumnCount();
-        String[] columnNames = new String[columnCount];
+    public void callProcedure(String procedure){
 
-        for (int i = 0; i < columnCount; i++) {
-            columnNames[i] = dbMetaData.getColumnName(i + 1); // Column index starts at 1
+        try {
+            dbStatement.executeQuery("CALL " + procedure);
+        } catch (Exception e) {
+            System.err.println("Procedure" + " Failed to Execute.");
+            e.printStackTrace();
         }
-        return columnNames;
     }
 
     public String[][] getAnimes(){
@@ -77,7 +95,6 @@ public class AnimeSystem {
         try {
             dbResultSet = dbStatement.executeQuery("SELECT title, genre, air_date FROM animes");
             dbMetaData = dbResultSet.getMetaData();
-            data.add(getColumnNames(dbMetaData));
             int columnCount = dbMetaData.getColumnCount();
             while (dbResultSet.next()) {
                 String[] rowData = new String[columnCount];
@@ -87,7 +104,7 @@ public class AnimeSystem {
                 data.add(rowData);
             }
         } catch (Exception e) {
-            System.err.println("Query to 'dbanime' Failed");
+            System.err.println("Query to 'dbanime' Failed.");
             e.printStackTrace();
         }
         return data.toArray(new String[0][0]);
