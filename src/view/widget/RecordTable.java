e1e95e6bce60e1c52829dafa8ebf42cdbb08f419
Justin Rainier Go <justinrainiergo@gmail.com>
2024-11-17 14:23:20 +0800

    Dynamically get table columns + autofit content

--- src/view/widget/RecordTable.java
+++ src/view/widget/RecordTable.java
@@ -1,17 +1,18 @@
 package src.view.widget;
 
+import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
 
-import javax.swing.BoxLayout;
 import javax.swing.JDialog;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.ScrollPaneConstants;
-import javax.swing.table.JTableHeader;
+import javax.swing.table.TableCellRenderer;
+import javax.swing.table.TableColumn;
 import javax.swing.JButton;
 
 import src.controller.RecordTableListener;
@@ -23,10 +24,10 @@ public class RecordTable extends JDialog {
     private GridBagConstraints c = new GridBagConstraints();
 
     public RecordTable() {
-        this.setMinimumSize(new Dimension(800, 800));
+        this.setMinimumSize(new Dimension(1000, 1000));
         panel = new JPanel();
         panel.setLayout(new GridBagLayout());
-        c.insets = new Insets(16, 16, 16, 16);
+        c.insets = new Insets(16, 8, 16, 8);
         c.fill = GridBagConstraints.BOTH;
         c.gridx = 0;
         c.gridy = 0;
@@ -35,20 +36,37 @@ public class RecordTable extends JDialog {
     }
 
     public void setTableData(String[][] data, String[] columnNames) {
-        table = new JTable(data, columnNames);
+        table = new JTable(data, columnNames) {
+            @Override
+            public Component prepareRenderer(TableCellRenderer renderer, int rowIndex, int columnIndex) {
+                Component component = super.prepareRenderer(renderer, rowIndex, columnIndex);
+                TableColumn tableColumn = getColumnModel().getColumn(columnIndex);
+                int componentPreferredWidth = component.getPreferredSize().width;
+                int columnPreferredWidth = tableColumn.getPreferredWidth();
+                tableColumn.setPreferredWidth(Math.max(componentPreferredWidth, columnPreferredWidth));
+                return component;
+            }
+        };
         WidgetFactory.styleComponent(table);
-        JScrollPane scrollPane = new JScrollPane(table);
+        table.getTableHeader().setFont(WidgetFactory.Fonts.BODY.getFont());
+
+        JScrollPane scrollPane = new JScrollPane(
+                table,
+                ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS,
+                ScrollPaneConstants.HORIZONTAL_SCROLLBAR_ALWAYS);
         WidgetFactory.styleComponent(scrollPane);
-        table.setAutoResizeMode(JTable.AUTO_RESIZE_ALL_COLUMNS);
-        scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED);
-        scrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED);
-        panel.add(new JScrollPane(table), c);
+
+        int prevIpadx = c.ipadx;
+        c.ipadx = 400;
+        panel.add(scrollPane, c);
         c.gridy++;
         for (String[] row : data) {
             for (String cell : row) {
                 System.out.println(cell);
             }
         }
+        c.ipadx = prevIpadx;
+
         button = WidgetFactory.createJButton("Select");
         panel.add(button, c);
         c.gridy++;
