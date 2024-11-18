0a4ece898b5182b0018c52afe166c090a2d2b7dd
Johan Daniel Ang <johandanielang@gmail.com>
2024-11-18 23:00:03 +0800

    + Updated Anime Genres and Data

--- src/model/sql/dbanime.sql
+++ src/model/sql/dbanime.sql
@@ -5,7 +5,7 @@ USE `dbanime`;
 DROP TABLE IF EXISTS `users`;
 CREATE TABLE `users` (
 	`user_id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `user_name` VARCHAR(32) NOT NULL,
+    `user_name` VARCHAR(32) NOT NULL UNIQUE,
     `region` ENUM('JP', 'AM', 'EU', 'AS', 'AU', 'AF', 'AC') NOT NULL,
     -- REGIONS: Japan, Americas, Europe, Asia, Australia, Africa, Antarctica
     `join_date` DATE NOT NULL
@@ -14,7 +14,7 @@ CREATE TABLE `users` (
 DROP TABLE IF EXISTS `studios`;
 CREATE TABLE `studios` (
 	`studio_id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `studio_name` VARCHAR(32) NOT NULL
+    `studio_name` VARCHAR(32) NOT NULL UNIQUE
 );
 
 DROP TABLE IF EXISTS `staff`;
@@ -23,7 +23,8 @@ CREATE TABLE `staff` (
     `first_name` VARCHAR(16) NOT NULL,
     `last_name` VARCHAR(16) NOT NULL,
     `occupation` VARCHAR(32) NOT NULL,
-    `birthday` DATE NOT NULL
+    `birthday` DATE NOT NULL,
+    UNIQUE('first_name', 'last_name');
 );
 
 DROP TABLE IF EXISTS `animes`;
@@ -31,12 +32,10 @@ CREATE TABLE `animes` (
 	`anime_id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     `studio_id` INT NOT NULL,
     `title` VARCHAR(64) UNIQUE,
-    `genre` ENUM('AC', 'AD', 'CO', 'DR', 'FA', 'HO', 'MY', 'RO', 'SF', 'SL', 'SP', 'TH', 'SU') NOT NULL,
-    -- GENRES: Action, Adventure, Comedy, Drama, Fantasy, Horror, Mystery, Romance, Sci-Fi, Slice of Life, Sports, Thriller, Supernatural
+    `genre` ENUM('AD', 'CO', 'DR', 'HO', 'SF') NOT NULL,
+    -- GENRES: Adventure, Comedy, Drama, Horror, Sci-Fi
     `air_date` DATE NOT NULL,
     `num_of_episodes` INT DEFAULT 0,
-    `available_from_date` DATE NOT NULL,
-    `available_to_date` DATE NOT NULL,
     FOREIGN KEY(`studio_id`) REFERENCES `studios`(`studio_id`)
 );
 
@@ -59,190 +58,150 @@ CREATE TABLE `ratings` (
     `comment` VARCHAR(2048),
     `last_episode_watched` INT NOT NULL,
     `last_edited_timestamp` TIMESTAMP NOT NULL,
+    PRIMARY KEY(`user_id`, `anime_id`),
     FOREIGN KEY(`user_id`) REFERENCES `users`(`user_id`),
     FOREIGN KEY(`anime_id`) REFERENCES `animes`(`anime_id`)
 );
 
 DROP TABLE IF EXISTS `credits`;
 CREATE TABLE `credits` (
-	`staff_id` INT NOT NULL PRIMARY KEY,
+	`staff_id` INT NOT NULL,
     `anime_id` INT NOT NULL,
     `position` VARCHAR(32) NOT NULL,
     `department` ENUM('DP', 'AD', 'AN', 'EV', 'SS', 'TO') NOT NULL,
     -- DEPARTMENTS: Direction and Production, Art and Design, Animation, Sound and Music, Editing and Visual Effects, Script and Storyboarding, Technical and Other Staff
+    PRIMARY KEY(`staff_id`, `anime_id`),
     FOREIGN KEY(`staff_id`) REFERENCES `staff`(`staff_id`),
     FOREIGN KEY(`anime_id`) REFERENCES `animes`(`anime_id`)
 );
 
 DROP TABLE IF EXISTS `follows`;
 CREATE TABLE `follows` (
-	`follower_id` INT NOT NULL PRIMARY KEY,
+	`follower_id` INT NOT NULL,
     `followed_id` INT NOT NULL,
     `following_since_date` DATE NOT NULL,
+    PRIMARY KEY(`follower_id`, `followed_id`),
     FOREIGN KEY(`follower_id`) REFERENCES `users`(`user_id`),
     FOREIGN KEY(`followed_id`) REFERENCES `users`(`user_id`)
 );
 
--- Insert sample data into `users` table
+INSERT INTO `studios` (`studio_id`, `studio_name`) VALUES
+('Studio Zero'),            -- 1
+('StarVision Studios'),     -- 2
+('PixelCraft Studios'),     -- 3
+('Dreamscape Productions'), -- 4
+('NebulaWorks');            -- 5
+
 INSERT INTO `users` (`user_name`, `region`, `join_date`) VALUES
-('AnimeFan88', 'JP', '2023-01-10'),
-('PirateKingBob', 'AM', '2023-05-15'),
-('MysticCharlie', 'EU', '2023-07-20'),
-('DragonQueenDiana', 'AS', '2023-02-25'),
-('AussieOtaku', 'AU', '2023-03-30'),
-('SafariFiona', 'AF', '2023-08-15'),
-('GundamGeorge', 'EU', '2023-06-10'),
-('SpaceCowgirl', 'AM', '2023-04-05'),
-('Ninja_Ivy', 'AS', '2023-09-12'),
-('KitsuneJack', 'JP', '2023-10-01'),
-('Tohru_Adachi_From_P4', 'JP', '2024-01-01');
+('SunshineSoda',   'JP', '2022-01-02'), -- 1
+('Periwinkle',     'EU', '2022-02-03'), -- 2
+('PixelPirate',    'AM', '2022-04-22'), -- 3
+('MysticBlaze',    'AS', '2022-04-30'), -- 4
+('WaffleQuest',    'AU', '2022-05-12'); -- 5
 
--- Insert sample data into `studios` table
-INSERT INTO `studios` (`studio_name`) VALUES
-('Studio Ghibli'),
-('Toei Animation'),
-('Madhouse'),
-('Bones'),
-('Sunrise'),
-('Kyoto Animation'),
-('A-1 Pictures'),
-('MAPPA'),
-('Studio Trigger'),
-('CloverWorks');
+INSERT INTO `animes` (`studio_id`, `title`, `genre`, `air_date`, `num_of_episodes`) VALUES
+(1, 'Eternal Horizon',       'AC', '2022-01-06', 3), -- 1
+(2, 'Violet Shards',         'DR', '2022-03-25', 2), -- 2
+(2, 'Cosmic Eclipse',        'SF', '2022-04-15', 5), -- 3
+(3, 'Wanderlust Chronicles', 'DR', '2023-03-03', 3), -- 4
+(5, 'Unspoken Truth',        'AC', '2023-07-09', 2), -- 5
+(4, 'Whispers of the Past',  'HO', '2023-09-03', 3), -- 6
+(4, 'Phantom Dance',         'CO', '2023-12-23', 1); -- 7
 
--- Insert sample data into `staff` table
-INSERT INTO `staff` (`first_name`, `last_name`, `occupation`, `birthday`) VALUES
-('Hayao', 'Miyazaki', 'Director', '1941-01-05'),
-('Isao', 'Takahata', 'Producer', '1935-10-29'),
-('Yoko', 'Kanno', 'Composer', '1964-03-18'),
-('Makoto', 'Shinkai', 'Director', '1973-02-09'),
-('Yoshiyuki', 'Tomino', 'Animator', '1941-11-05'),
-('Satoshi', 'Kon', 'Screenwriter', '1963-10-12'),
-('Mamoru', 'Hosoda', 'Director', '1967-09-19'),
-('Gen', 'Urobuchi', 'Writer', '1972-12-20'),
-('Naoko', 'Yamada', 'Animator', '1984-11-28'),
-('Shinichiro', 'Watanabe', 'Director', '1965-05-24');
-
--- Insert sample data into `animes` table
-INSERT INTO `animes` (`studio_id`, `title`, `genre`, `air_date`, `num_of_episodes`, `available_from_date`, `available_to_date`) VALUES
-(1, 'My Neighbor Totoro', 'FA', '1988-04-16', 1, '2023-01-01', '2023-12-31'),
-(2, 'One Piece', 'AD', '1999-10-20', 1000, '2023-01-01', '2023-12-31'),
-(3, 'Death Note', 'TH', '2006-10-04', 37, '2023-01-01', '2023-12-31'),
-(4, 'Fullmetal Alchemist', 'AC', '2003-10-04', 51, '2023-01-01', '2023-12-31'),
-(5, 'Cowboy Bebop', 'SF', '1998-04-03', 26, '2023-01-01', '2023-12-31'),
-(6, 'Attack on Titan', 'AC', '2013-04-06', 75, '2023-01-01', '2023-12-31'),
-(7, 'Neon Genesis Evangelion', 'MY', '1995-10-04', 26, '2023-01-01', '2023-12-31'),
-(8, 'Your Name', 'RO', '2016-08-26', 1, '2023-01-01', '2023-12-31'),
-(9, 'Demon Slayer', 'AD', '2019-04-06', 26, '2023-01-01', '2023-12-31'),
-(10, 'Violet Evergarden', 'DR', '2018-01-11', 13, '2023-01-01', '2023-12-31'),
-(1, 'Dragon Ball Z', 'AC', '1989-04-26', 291, '2023-01-01', '2023-12-31'),
-(2, 'Naruto', 'AD', '2002-10-03', 220, '2023-01-01', '2023-12-31'),
-(3, 'Sword Art Online', 'SF', '2012-07-08', 25, '2023-01-01', '2023-12-31'),
-(4, 'Jujutsu Kaisen', 'AC', '2020-10-03', 24, '2023-01-01', '2023-12-31'),
-(5, 'Hunter x Hunter', 'AD', '2011-10-02', 148, '2023-01-01', '2023-12-31'),
-(6, 'Mob Psycho 100', 'CO', '2016-07-12', 25, '2023-01-01', '2023-12-31'),
-(7, 'Steins;Gate', 'TH', '2011-04-06', 24, '2023-01-01', '2023-12-31'),
-(8, 'Tokyo Ghoul', 'HO', '2014-07-04', 12, '2023-01-01', '2023-12-31'),
-(9, 'Black Clover', 'FA', '2017-10-03', 170, '2023-01-01', '2023-12-31'),
-(10, 'Fruits Basket', 'SL', '2019-04-06', 63, '2023-01-01', '2023-12-31'),
-(7, 'Persona 4 Golden Animation', 'SU','2011-10-07', 26,'2023-01-01', '2023-12-31' );
-
--- Insert sample data into `views` table
 INSERT INTO `views` (`user_id`, `anime_id`, `watched_episode`, `timestamp_watched`) VALUES
-(1, 1, 1, '2023-01-11 10:30:00'),
-(2, 2, 10, '2023-05-16 12:45:00'),
-(2, 2, 11, '2023-05-16 13:45:00'),
-(2, 2, 12, '2023-05-16 14:45:00'),
-(2, 2, 13, '2023-05-16 15:45:00'),
-(2, 2, 14, '2023-05-16 16:45:00'),
-(3, 3, 5, '2023-07-21 15:15:00'),
-(4, 4, 15, '2023-02-26 16:30:00'),
-(5, 5, 20, '2023-04-01 18:00:00'),
-(6, 6, 30, '2023-09-01 13:00:00'),
-(7, 7, 2, '2023-06-12 09:45:00'),
-(8, 8, 5, '2023-04-07 19:30:00'),
-(9, 9, 18, '2023-09-15 11:00:00'),
-(10, 10, 1, '2023-10-02 17:45:00'),
-(1, 11, 1, '2023-01-20 11:15:00'),
-(2, 12, 5, '2023-06-15 14:20:00'),
-(3, 13, 10, '2023-08-22 12:10:00'),
-(4, 14, 8, '2023-03-05 17:00:00'),
-(5, 15, 6, '2023-04-10 18:30:00'),
-(6, 16, 3, '2023-09-20 09:00:00'),
-(7, 17, 15, '2023-06-25 10:30:00'),
-(8, 18, 18, '2023-05-01 20:15:00'),
-(9, 19, 7, '2023-10-10 13:20:00'),
-(10, 20, 2, '2023-10-15 16:45:00'),
-(1, 2, 25, '2023-02-10 12:00:00'),
-(2, 3, 10, '2023-05-18 15:40:00'),
-(3, 4, 30, '2023-09-05 11:30:00'),
-(4, 5, 18, '2023-03-10 18:45:00'),
-(5, 6, 50, '2023-04-12 19:00:00'),
-(6, 7, 15, '2023-10-01 14:30:00'),
-(7, 8, 1, '2023-06-15 16:45:00'),
-(8, 9, 7, '2023-07-05 20:30:00'),
-(9, 10, 12, '2023-11-01 14:20:00'),
-(10, 1, 3, '2023-10-12 10:10:00'),
-(1, 11, 2, '2023-02-25 13:15:00'),
-(2, 12, 12, '2023-06-20 14:55:00'),
-(3, 13, 24, '2023-09-10 12:40:00'),
-(4, 14, 20, '2023-03-20 17:15:00'),
-(5, 15, 13, '2023-05-10 18:55:00');
+-- USER 1 (SunshineSoda)
+(1, 1, 1, '2022-01-06 03:22:54'),
+(1, 1, 2, '2022-01-06 04:50:21'),
+(1, 1, 3, '2022-01-09 08:00:11'),
+(1, 1, 1, '2022-02-11 16:08:31'),
+(1, 1, 2, '2022-02-12 17:05:23'),
+(1, 1, 3, '2022-02-15 12:07:10'),
+(1, 3, 1, '2022-07-01 09:01:53'),
+(1, 3, 2, '2022-07-10 12:19:20'),
+(1, 2, 1, '2023-01-14 16:20:08'),
+-- USER 2 (Periwinkle)
+(2, 2, 1, '2022-06-14 11:21:00'),
+(2, 2, 2, '2022-06-14 12:32:05'),
+(2, 7, 1, '2023-12-24 06:58:38'),
+-- USER 3 (PixelPirate)
+(3, 5, 1, '2023-07-11 12:03:06'),
+(3, 5, 2, '2023-07-12 05:10:00'),
+(3, 1, 1, '2023-08-03 03:04:56'),
+(3, 6, 1, '2023-09-08 02:01:01'),
+(3, 6, 2, '2023-11-02 19:07:30'),
+(3, 6, 3, '2023-11-21 23:22:12'),
+-- USER 4 (MysticBlaze)
+(4, 2, 2, '2022-07-21 16:29:10'),
+(4, 4, 1, '2023-03-03 01:58:26'),
+(4, 4, 2, '2023-03-04 00:21:02'),
+(4, 4, 3, '2023-05-07 16:08:14'),
+-- USER 5 (WaffleQuest)
+(5, 5, 1, '2023-07-09 12:34:42'),
+(5, 5, 2, '2023-07-15 11:21:11'),
+(5, 2, 1, '2023-09-11 09:42:02'),
+(5, 1, 1, '2023-10-12 23:38:53'),
+(5, 3, 1, '2023-10-13 20:57:08'),
+(5, 3, 2, '2023-10-14 16:58:21'),
+(5, 3, 1, '2023-10-15 15:30:00');
 
--- Insert sample data into `ratings` table
 INSERT INTO `ratings` (`user_id`, `anime_id`, `rating`, `comment`, `last_episode_watched`, `last_edited_timestamp`) VALUES
-(1, 1, 5, 'Incredible animation and story.', 1, '2023-01-12 09:30:00'),
-(2, 2, 4, 'Great adventure with amazing characters.', 100, '2023-05-20 11:00:00'),
-(3, 3, 5, 'Dark and thrilling.', 37, '2023-07-25 13:30:00'),
-(4, 4, 3, 'Interesting, but a bit slow.', 20, '2023-02-28 17:00:00'),
-(5, 5, 5, 'Sci-fi masterpiece.', 26, '2023-04-03 18:45:00'),
-(6, 6, 4, 'Amazing animation.', 60, '2023-09-02 14:00:00'),
-(7, 7, 5, 'Mind-bending classic.', 26, '2023-06-13 10:30:00'),
-(8, 8, 4, 'Emotional and beautifully animated.', 1, '2023-04-08 20:00:00'),
-(9, 9, 5, 'Epic fights and storyline.', 26, '2023-09-16 11:45:00'),
-(10, 10, 4, 'Touching and visually stunning.', 13, '2023-10-03 18:15:00'),
-(1, 11, 4, 'Classic action scenes.', 10, '2023-01-25 11:00:00'),
-(2, 12, 5, 'Beautiful storytelling.', 220, '2023-06-18 15:00:00'),
-(3, 13, 5, 'Engaging plot twists.', 24, '2023-08-25 12:20:00'),
-(4, 14, 3, 'Solid story but slow pacing.', 10, '2023-03-10 18:00:00'),
-(5, 15, 4, 'Intense battles.', 6, '2023-04-15 18:45:00'),
-(6, 16, 5, 'Top-tier animation and plot.', 148, '2023-09-20 09:10:00'),
-(7, 17, 4, 'Funny and thrilling.', 25, '2023-06-30 10:00:00'),
-(8, 18, 5, 'A masterpiece.', 24, '2023-05-10 20:30:00'),
-(9, 19, 3, 'Not as good as expected.', 12, '2023-10-11 13:00:00'),
-(10, 20, 4, 'Surprisingly good.', 63, '2023-10-20 16:00:00');
+-- USER 1 (SunshineSoda)
+(1, 1, 5, 'Amazing anime, love the characters! In fact, I watched it again!', 3, '2022-02-16 12:20:08'),
+(1, 3, 2, 'Kinda mid, didn\'t really end up liking it at the end.',           2, '2022-07-10 18:12:01'),
+(1, 2, 4, 'I love the color purple!',                                         1, '2023-02-15 06:02:09'),
+-- USER 2 (Periwinkle)
+(2, 2, 5, 'Periwinkle Periwinkle!',                                           2, '2022-06-15 03:01:13'),
+(2, 7, 4, 'Very slay!',                                                       1, '2023-12-24 07:08:19'),
+-- USER 3 (PixelPirate)
+(3, 5, 4, 'Loved the overall storyline and visuals! Great job NebulaWorks',   2, '2023-07-12 09:11:02'),
+(3, 1, 4, 'Great anime, great visuals, love it so far!',                      1, '2023-08-04 16:07:22'),
+(3, 6, 1, 'Didn\'t really like it, hope they improve on the next season',     2, '2023-11-02 21:34:31'),
+-- USER 4 (MysticBlaze)
+(4, 2, 1, 'Lacked a bit of depth. Could\'ve been better imo.',                1, '2022-07-22 06:45:00'),
+(4, 4, 5, 'I\'ve got no words for how great this anime is. MAN',              2, '2023-04-01 00:11:23'),
+-- USER 5 (WaffleQuest)
+(5, 5, 1, 'Action scenes feel very empty and plain. Could be so much better', 1, '2023-07-14 12:23:57'),
+(5, 2, 5, 'I really do love the color purple tbh. That\'s all I can say :D',  1, '2023-09-12 14:31:08'),
+(5, 1, 3, 'Didn\'t meet my expectations despite how my friends described it', 1, '2023-10-13 21:42:34'),
+(5, 3, 5, 'I love sci-fi and boy did i love this anime! :DDDDDDD',            2, '2023-10-14 17:08:11');
 
--- Insert sample data into `credits` table
+INSERT INTO `staff` (`first_name`, `last_name`, `occupation`, `birthday`) VALUES
+('Hiroshi', 'Takeda',    'Director',       '1975-02-10'), -- 1
+('Maya',    'Shirogane', 'Animator',       '1981-07-22'), -- 2
+('Kenji',   'Fujimoto',  'Sound Engineer', '1990-11-30'), -- 3
+('Rika',    'Sakamoto',  'Producer',       '1986-01-15'), -- 4
+('Yuki',    'Hamada',    'Writer',         '1993-09-09'), -- 5
+('Takumi',  'Sato',      'Art Director',   '1987-04-04'), -- 6
+('Ayumi',   'Kobayashi', 'Music Composer', '1992-12-17'), -- 7
+('Daichi',  'Tanaka',    'Editor',         '1985-06-13'); -- 8
+
 INSERT INTO `credits` (`staff_id`, `anime_id`, `position`, `department`) VALUES
 (1, 1, 'Director', 'DP'),
+(1, 7, 'Director', 'DP'),
 (2, 1, 'Producer', 'DP'),
-(3, 5, 'Composer', 'SS'),
-(4, 4, 'Director', 'DP'),
-(5, 2, 'Animator', 'AN'),
-(6, 6, 'Director', 'DP'),
-(7, 7, 'Writer', 'SS'),
-(8, 3, 'Screenwriter', 'SS'),
-(9, 10, 'Animator', 'AN'),
-(10, 8, 'Director', 'DP');
+(2, 4, 'Producer', 'DP'),
+(3, 2, 'Animator', 'AN'),
+(4, 3, 'Sound Engineer', 'SS'),
+(4, 6, 'Sound Engineer', 'SS'),
+(5, 4, 'Scriptwriter', 'TO'),
+(6, 5, 'Art Designer', 'AD'),
+(7, 6, 'Editor', 'EV'),
+(8, 7, 'Animator', 'AN');
 
--- Insert sample data into `follows` table
 INSERT INTO `follows` (`follower_id`, `followed_id`, `following_since_date`) VALUES
-(1, 2, '2023-01-15'),
-(2, 3, '2023-06-01'),
-(3, 4, '2023-08-01'),
-(4, 5, '2023-03-01'),
-(5, 1, '2023-04-10'),
-(6, 7, '2023-08-15'),
-(7, 8, '2023-06-20'),
-(8, 9, '2023-04-10'),
-(9, 10, '2023-09-13'),
-(10, 1, '2023-10-05');
+(1, 2, '2022-02-03'),
+(2, 1, '2022-02-04'),
+(3, 2, '2022-06-10'),
+(2, 4, '2022-07-07'),
+(5, 3, '2023-01-10'),
+(1, 4, '2023-02-20'),
+(4, 3, '2023-03-01');
 
 -- ===================================REPORT PROCEDURES===================================
 
 -- PROCEDURE
 -- Usage "CALL SelectBestAnimeOverall()"
--- Creates a table called `best_anime` for overall besst anime based on rating
+-- Creates a table called `best_anime` for overall best anime based on rating
 DELIMITER //
 -- DROP PROCEDURE IF EXISTS `SelectBestAnimeOverall`; 
 CREATE PROCEDURE `SelectBestAnimeOverall`()
