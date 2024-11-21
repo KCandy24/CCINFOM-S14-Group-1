4ceb8bd30ddd23deb0e92b5cebfc27477b79768f
Johan Daniel Ang <johandanielang@gmail.com>
2024-11-21 19:06:35 +0800

    + Added functionality to anime records

--- src/controller/RecordsTabListener.java
+++ src/controller/RecordsTabListener.java
@@ -2,6 +2,7 @@ package src.controller;
 
 import java.awt.event.*;
 import java.sql.SQLException;
+import java.time.LocalDate;
 
 import javax.swing.JComponent;
 
@@ -108,26 +109,77 @@ public class RecordsTabListener implements ActionListener {
     public void searchAnime() {
         topView.selectFromTable("animes");
 
-        // TODO: ENABLE DELETE BUTTON IF DISABLED
+        topView.getComponent(TopView.RECORDS_TAB, TopView.ANIME_RECORD_SUBTAB, "deleteAnime").setEnabled(true);
     }
 
     public void addNewAnime() {
+        Subtab subtab = topView.getSubtab(TopView.RECORDS_TAB, TopView.ANIME_RECORD_SUBTAB);
         topView.resetFields(TopView.RECORDS_TAB, TopView.ANIME_RECORD_SUBTAB);
 
-        // TODO: DISABLE DELETE BUTTON
+        subtab.setComponentText(topView.getComponent(TopView.RECORDS_TAB, TopView.ANIME_RECORD_SUBTAB, "airDate"), String.valueOf(LocalDate.now()));
+
+        topView.getComponent(TopView.RECORDS_TAB, TopView.ANIME_RECORD_SUBTAB, "deleteAnime").setEnabled(false);
     }
 
     public void saveAnime() {
-        // TODO: IMPLEMENTATION
+        Subtab subtab = topView.getSubtab(TopView.RECORDS_TAB, TopView.ANIME_RECORD_SUBTAB);
+        String animeId = subtab.getComponentText("animeId");
+        String studioId = subtab.getComponentText("studioId");
+        String animetitle = subtab.getComponentText("animeTitle");
+        String genre = subtab.getComponentText("genre", "genre");
+        String airDate = subtab.getComponentText("airDate");
+        String episodes = subtab.getComponentText("episodes");
 
-        // TODO: ENABLE DELETE BUTTON IF DISABLED
+        try {
+            Integer.parseInt(animeId);
+            updateAnime(animeId, studioId, animetitle, genre, airDate, episodes);
+        } catch (NumberFormatException exception) {
+            createAnime(studioId, animetitle, genre, episodes);
+        }
+        
+        topView.getComponent(TopView.RECORDS_TAB, TopView.ANIME_RECORD_SUBTAB, "deleteAnime").setEnabled(true);
+    }
+
+    public void createAnime(String studioId, String animeTitle, String genre, String episodes) {
+        try {
+            String query = """
+                    INSERT INTO `animes` (`studio_id`, `title`, `genre`, `air_date`, `num_of_episodes`) VALUES
+                    (?, ?, ?, NOW(), ?)
+                    """;
+            animeSystem.safeUpdate(query, studioId, animeTitle, genre, episodes);
+        } catch (SQLException exception) {
+            topView.dialogPopUp("SQLException", exception.getMessage());
+        }
+    }
+
+    public void updateAnime(String animeId, String studioId, String animeTitle, String genre, String airDate, String episodes) {
+        try {
+            String query = """
+                    UPDATE  `animes`
+                    SET     `studio_id` = ?,
+                    `title` = ?,
+                    `genre` = ?,
+                    `air_date` = ?,
+                    `num_of_episodes` = ?
+                    WHERE `anime_id` = ?
+                    """;
+            animeSystem.safeUpdate(query, studioId, animeTitle, genre, airDate, episodes, animeId);
+        } catch (SQLException exception) {
+            topView.dialogPopUp("SQLException", exception.getMessage());
+        }
     }
 
     public void deleteAnime() {
-        topView.resetFields(TopView.RECORDS_TAB, TopView.ANIME_RECORD_SUBTAB);
+        Subtab subtab = topView.getSubtab(TopView.RECORDS_TAB, TopView.ANIME_RECORD_SUBTAB);
+        String animeId = subtab.getComponentText("animeId");
 
-        // TODO: IMPLEMENTATION
-        // TODO: DISABLE DELETE BUTTON
+        try {
+            animeSystem.safeUpdate("DELETE FROM `animes` WHERE `anime_id` = ?", animeId);
+        } catch (SQLException exception) {
+            topView.dialogPopUp("SQLException", exception.getMessage());
+        }
+
+        topView.getComponent(TopView.RECORDS_TAB, TopView.ANIME_RECORD_SUBTAB, "deleteAnime").setEnabled(false);
     }
 
     // User records management
@@ -135,13 +187,13 @@ public class RecordsTabListener implements ActionListener {
     public void searchUser() {
         topView.selectFromTable("users");
 
-        // TODO: ENABLE DELETE BUTTON IF DISABLED
+        topView.getComponent(TopView.RECORDS_TAB, TopView.ANIME_RECORD_SUBTAB, "deleteUser").setEnabled(true);
     }
 
     public void addNewUser() {
         topView.resetFields(TopView.RECORDS_TAB, TopView.USER_RECORD_SUBTAB);
 
-        // TODO: DISABLE DELETE BUTTON
+        topView.getComponent(TopView.RECORDS_TAB, TopView.ANIME_RECORD_SUBTAB, "deleteUser").setEnabled(false);
     }
 
     /**
@@ -161,7 +213,8 @@ public class RecordsTabListener implements ActionListener {
             // This must be a new record
             createUser(username, region, joinDate);
         }
-        // TODO: ENABLE DELETE BUTTON IF DISABLED
+
+        topView.getComponent(TopView.RECORDS_TAB, TopView.ANIME_RECORD_SUBTAB, "deleteUser").setEnabled(true);
     }
 
     public void createUser(String username, String region, String joinDate) {
@@ -193,6 +246,8 @@ public class RecordsTabListener implements ActionListener {
         } catch (SQLException exception) {
             topView.dialogPopUp("SQLException", exception.getMessage());
         }
+
+        topView.getComponent(TopView.RECORDS_TAB, TopView.ANIME_RECORD_SUBTAB, "deleteUser").setEnabled(false);
     }
 
     // Staff records management
